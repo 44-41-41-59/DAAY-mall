71416bdd41389a643d88263ec8cd9178969aa4d9
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-06-21 13:00:35 +0300

    payment history routes and handlers with model and schema done

---
+++ src/DB/users/payment-history/payment-history.model.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const paymentHistorySchema=require('./payment-history.schema.js');
+
+class paymentHistoryModel {
+  constructor(schema) {
+    this.schema = schema;
+  }
+
+  async read(obj) {
+    if (obj === undefined) {
+      return await this.schema.find({}).populate('productID');
+    }
+    return await this.schema.find(obj).populate('productID');
+  }
+
+  create(record) {
+    let newRecord = new this.schema(record);
+    return newRecord.save();
+  }
+
+  update(_id, record) {
+    return this.schema.findByIdAndUpdate(_id, record, { new: true }).populate('productID');
+  }
+
+  delete(_id) {
+    return this.schema.findByIdAndDelete(_id);
+  }
+}
+
+module.exports = new paymentHistoryModel(paymentHistorySchema);
