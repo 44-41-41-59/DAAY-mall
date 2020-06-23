4aecb27c5931984a23b47b3a4a542eb5f0bd31e1
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-06-23 09:51:40 +0300

    step6

--- src/DB/cart/cart-schema.js
+++ src/DB/cart/cart-schema.js
@@ -5,6 +5,7 @@ const products = require('../product/product-schema.js');
 const Cart = Schema({
   userID: { type: Schema.Types.ObjectId },
   products: { type: Schema.Types.ObjectId, ref: 'product' },
+  quantity: { type: Number, default: 1 },
 });
 
 // Cart.virtual('carts', {
